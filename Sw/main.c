f832fb285b815950be34ea1e80b22a8ca8814b60
Eduardo Heredia Gonzalez <EduardoHerediaGonzalez@gmail.com>
2024-04-28 16:25:02 -0600

    Deleting ASSERT component folder due to duplicaten.

--- Sw/main.c
+++ Sw/main.c
@@ -18,9 +18,6 @@
 
 #include <stdint.h>
 
-#include "RCC_def.h"
-#include "RCC.h"
-
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
@@ -28,7 +25,5 @@
 int main(void)
 {
     /* Loop forever */
-
-
 	for(;;);
 }
